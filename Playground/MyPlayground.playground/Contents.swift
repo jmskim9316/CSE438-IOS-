b1cab596040e3175ea449f562562bc0b54057959
Min Seop Kim <mkim39@wustl.edu>
2018-06-13 14:45:56 -0500

    Commit test

---
+++ Playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+print("hi")
+
+for num in 1...5 {
+    print("\(num) times 4 is \(num*4)")
+}
+
+var myVariable = 42
+myVariable = 50
+
+let implicitInt = 70
+let implicitDoub = 70.0
+let explicitDoub:Double = 70
+
+
+
+
